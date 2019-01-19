b73f9ff2b166f06afe1d5e65d21dbb03feeb7eba
Hudson da Silva Xavier <hudson3101@yahoo.com.br>
2019-01-19 13:24:56 -0200

    - Novo Commit de teste;

---
+++ Services/ViaCEP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Services
+{
+    public class ViaCEP
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json";
+
+        public static DataAccess.Endereco BuscarEnderecoVIACEP(string cep)
+        {
+            string NovoEndereco = String.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(NovoEndereco);
+
+            DataAccess.Endereco end = JsonConvert.DeserializeObject<DataAccess.Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
